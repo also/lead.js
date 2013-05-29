7878db7b436e00d5bb674937d5ee7c4f7b9bf45e
Ryan Berdeen <rberdeen@hubspot.com>
2013-05-29 17:36:30 -0400

    include show-hint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //= require ./lib/codemirror-3.12/javascript.js
 //= require ./lib/codemirror-3.12/coffeescript.js
 //= require ./lib/codemirror-3.12/runmode.js
+//= require ./lib/codemirror-3.12/show-hint.js
 //= require ./lib/stacktrace-min-0.4.js
 //= require ./core.js
 //= require ./functions.js
