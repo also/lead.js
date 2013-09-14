0338851ddd1c3ac52fa1af0f573669726c333199
Ryan Berdeen <ryan@ryanberdeen.com>
2013-09-13 21:54:30 -0400

    formatting

--- spec/phantom.js
+++ spec/phantom.js
@@ -8,38 +8,37 @@ var url = 'http://localhost:9292/runner.html';
 page.open(url);
 
 function configureJasmine(env, phantom) {
-	reporter = new jasmine.Reporter();
-	reporter.reportRunnerResults = function(runner) {
-  		var results = runner.results();
-  		
+  reporter = new jasmine.Reporter();
+  reporter.reportRunnerResults = function(runner) {
+      var results = runner.results();
 
-  		window.callPhantom(results);
-	};
-	env.addReporter(reporter);
+      window.callPhantom(results);
+  };
+  env.addReporter(reporter);
 }
 
 page.onInitialized = function() {
-	page.evaluate(function(configureJasmine, phantom) {
-		window.configure_jasmine = function(env) { configureJasmine(env, phantom) };
-	}, configureJasmine, phantom);
+  page.evaluate(function(configureJasmine, phantom) {
+    window.configure_jasmine = function(env) { configureJasmine(env, phantom) };
+  }, configureJasmine, phantom);
 };
 
 page.onCallback = function(results) {
-	console.log("total:  ", results.totalCount);
-	console.log("passed: ", results.passedCount);
-	console.log("failed: ", results.failedCount);
+  console.log("total:  ", results.totalCount);
+  console.log("passed: ", results.passedCount);
+  console.log("failed: ", results.failedCount);
 
-	if (results.failedCount > 0) {
-		phantom.exit(1);
-	}
-	else {
-		phantom.exit(0);
-	}
+  if (results.failedCount > 0) {
+    phantom.exit(1);
+  }
+  else {
+    phantom.exit(0);
+  }
 };
 
 function timeout() {
-	console.log('tests failed to complete');
-	phantom.exit(2);
+  console.log('tests failed to complete');
+  phantom.exit(2);
 }
 
 setTimeout(timeout, 10000);
