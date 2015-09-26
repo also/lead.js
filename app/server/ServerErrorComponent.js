b2ab5065723ecd8ca5d479a120c17b5c385e71de
Ryan Berdeen <ryan@ryanberdeen.com>
2015-09-26 16:10:47 -0400

    remove old coffeescript comment

--- app/server/ServerErrorComponent.js
+++ app/server/ServerErrorComponent.js
@@ -6,19 +6,6 @@ import {ToggleComponent} from '../builtins';
 
 import ServerExceptionDetailsComponent from './ServerExceptionDetailsComponent';
 
-// ServerErrorComponent = React.createClass
-//   render: ->
-//     if _.isArray @props.error
-//       body = _.map @props.error, (exception) -> ServerExceptionDetailsComponent {exception}
-//     else if @props.error['unhandled-exception']
-//       body = ServerExceptionDetailsComponent {exception: @props.error['unhandled-exception']}
-//
-//     React.DOM.div {},
-//       React.DOM.strong {}, 'Server Error',
-//         body
-//         Components.ToggleComponent {title: 'Details'},
-//           Builtins.ObjectBrowserComponent {object: @props.error, showProto: false}
-//
 
 export default React.createClass({
   render() {
